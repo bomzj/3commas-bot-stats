1aa7899aac42f29119af22d13e76a8dc29ca901b
Maksim Shamihulau <maksim.shamihulau@gmail.com>
2022-05-21 21:30:45 +0200

    fix vite build to support top level await

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -13,6 +15,10 @@ export default defineConfig({
       sassVariables: 'src/quasar-variables.sass'
     })
   ],
+
+  build: {
+    target: 'esnext'
+  },
 
   server: {
     proxy: {
